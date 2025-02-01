e97d78ea789761c1c096dedc3853418fd7a3366a
rigon <2412789408@qq.com>
2025-02-01 15:57:04 +0800

    支持http协议

--- main/main.go
+++ main/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	gee_rpc "geerpc"
 	"log"
 	"net"
+	"net/http"
 	"sync"
 	"time"
 )
@@ -19,27 +21,17 @@ func (f Foo) Sum(args Args, reply *int) error {
 	return nil
 }
 
-func startServer(addr chan string) {
+func startServer(addrCh chan string) {
 	var foo Foo
-	if err := gee_rpc.Register(&foo); err != nil {
-		log.Fatal("register error:", err)
-	}
-	// pick a free port
-	l, err := net.Listen("tcp", ":0")
-	if err != nil {
-		log.Fatal("network error:", err)
-	}
-	log.Println("start rpc server on :", l.Addr())
-	addr <- l.Addr().String()
-	gee_rpc.Accept(l)
+	l, _ := net.Listen("tcp", ":9999")
+	_ = gee_rpc.Register(&foo)
+	gee_rpc.HandleHTTP()
+	addrCh <- l.Addr().String()
+	_ = http.Serve(l, nil)
 }
 
-func main() {
-	log.SetFlags(0)
-	addr := make(chan string)
-	go startServer(addr)
-
-	client, _ := gee_rpc.Dial("tcp", <-addr)
+func call(addrCh chan string) {
+	client, _ := gee_rpc.DialHTTP("tcp", <-addrCh)
 	defer func() {
 		_ = client.Close()
 	}()
@@ -56,11 +48,18 @@ func main() {
 				Num2: i * i,
 			}
 			var reply int
-			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+			if err := client.Call(context.Background(), "Foo.Sum", args, &reply); err != nil {
 				log.Fatal("call Foo.Sum error:", err)
 			}
 			log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
 		}(i)
 	}
 	wg.Wait()
+}
+
+func main() {
+	log.SetFlags(0)
+	ch := make(chan string)
+	go call(ch)
+	startServer(ch)
 }
